b2bb022fe4cfea50424b14fe49717aaac2ba8f6d
King <quantp@smartosc.com>
2019-01-16 17:22:38 +0700

    First Commit

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="form-container__title">Current Weather</h1>
+		<h3 className="form-container__subtitle">Find out temperature, conditions and more...</h3>
+	</div>
+);
+
+export default Titles;
